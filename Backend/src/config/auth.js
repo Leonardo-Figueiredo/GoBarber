32948febc7c993c5cce7b6be8752b0a4534d7318
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2020-02-05 16:53:54 -0300

    Authentication Middleware and Session added almost finalized

---
+++ Backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'c518013077ab7d0a6a428e13a81f719e',
+  expiresIn: '7d',
+};
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import sessionController from './app/controllers/SessionControler';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', sessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 module.exports = routes;
