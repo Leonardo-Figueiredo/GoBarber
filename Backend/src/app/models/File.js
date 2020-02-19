10fe853c683bed7dae5ecd0f58e9b63845ffa61d
Leonardo Figueiredo <leo.nardorf22@gmail.com>
2020-02-19 16:20:30 -0300

    Appointment Model and Migration

--- Backend/src/app/models/File.js
+++ Backend/src/app/models/File.js
@@ -9,7 +9,7 @@ class File extends Model {
         url: {
           type: Sequelize.VIRTUAL,
           get() {
-            return 'qualquer coisa';
+            return `http://localhost:3333/files/${this.path}`;
           },
         },
       },
