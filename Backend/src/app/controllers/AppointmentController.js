70153803827e016857a85d80719f87448a1af756
Leonardo-Figueiredo <leo.nardorf22@gmail.com>
2020-02-24 21:03:20 -0300

    MongoDB for Notifications, Schema and Notification Controller

--- Backend/src/app/controllers/AppointmentController.js
+++ Backend/src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import File from '../models/File';
 import User from '../models/User';
 import Appointment from '../models/Appointments';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -53,6 +55,12 @@ class AppointmentController {
 
     const { provider_id, date } = req.body;
 
+    if (provider_id === req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'User cannot create an appointment with himself' });
+    }
+
     /**
      *  Check if provider_id is a provider.
      */
@@ -95,6 +103,21 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    /**
+     * Notify appointment provider.
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
